987a4d8babcba2ae346be6eaeaede6703188a5f5
Nour Sherif Ali <108356194+Nour-Sherif-Ali@users.noreply.github.com>
2026-06-09 18:52:17 +0300

    Time: 55 ms (32.22%), Space: 94.2 MB (5.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
                     
-    bool containsDuplicate(vector<int> nums) {
-	unordered_set<int> dup;
-	
-	sort(nums.begin(), nums.end());  // [1,1,2,3]
-	for (int i = 0; i < nums.size(); i++)
-	{
-		if (dup.count(nums[i]))
-		{
-			return true;
-		}
-		dup.insert(nums[i]);
-	}
-	return false;
+    bool containsDuplicate(vector<int> nums) { // [1,2,3,1]  1 =2 , 2 =1 , 3 =1 
+	unordered_set<int> dup ; 
+    
+    for(int i = 0 ; i < nums.size() ; i++)
+    { 
+        if(dup.count(nums[i]))
+        {
+            return true ; 
+        }
+        dup.insert(nums[i]);   
+    }
+    return false;
 
 }
     
