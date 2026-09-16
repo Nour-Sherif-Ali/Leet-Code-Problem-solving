af1b58ece67351a420cf696a330345198b95460c
Nour Sherif Ali <108356194+Nour-Sherif-Ali@users.noreply.github.com>
2026-09-16 21:08:49 +0300

    Time: 0 ms (100%), Space: 17.7 MB (54.07%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    vector<int> memo ; 
+    int solve(vector<int>& cost, int n )
+    {
+        if(n == 0 || n == 1) return 0 ; 
+        if(memo[n] != -1) return memo[n];
+        int oneStep = solve(cost , n-1) + cost[n-1] ; 
+        int twoStep = solve(cost , n-2) + cost[n-2] ;
+
+        return memo[n] = min(oneStep , twoStep) ;  
+    }  
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size() ; 
-        vector<int> dp(n+1) ; 
-        for(int i = 2 ; i <= n ; i++)
-        {
-            dp[i] = min(dp[i - 1] + cost[i - 1] , dp[i-2] + cost[i-2]);
-        }
-        return dp[n]; 
+      int n = cost.size() ; 
+      memo.assign(n+1, -1); 
+      return solve(cost,n) ; 
     }
 };
